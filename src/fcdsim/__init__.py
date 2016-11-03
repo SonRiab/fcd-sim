064d6abc4c6a1fd259a626427bfd6438da15b343
Rene <rene@vnull.de>
2016-11-04 00:10:47 +0100

    [CHANGE] update help message for optional argument -f

--- src/fcdsim/__init__.py
+++ src/fcdsim/__init__.py
@@ -22,8 +22,8 @@ arg_parser.add_argument('-f', '--faulty-data-per-second',
                         type=int,
                         default=10,
                         metavar='NUM_FAULTY',
-                        help='The number of requests per second, which contains faulty Floating Car Data. This is \
-                        limited by the overall number of requests allowed. Default: 10')
+                        help='The maximum number of requests per second, which contains faulty Floating Car Data. \
+                        This is limited by the overall number of requests allowed. Default: 10')
 
 
 class FCDBase(object):
